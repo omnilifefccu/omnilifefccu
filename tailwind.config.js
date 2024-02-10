c747c052a0ac082a52d6713677e6479575754217
Shoaib Rashid <64620540+dptspartan@users.noreply.github.com>
2024-02-10 22:48:56 +0500

    tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.js",
+    "./src/Components/*.js",
+    "./src/Routes/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
